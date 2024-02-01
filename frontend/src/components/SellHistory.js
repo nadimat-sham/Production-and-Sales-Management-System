a7d16ee12b231c5709854345c5010a34ad751a01
nadimat-sham <1905059@ugrad.cse.buet.ac.bd>
2024-02-01 16:02:33 +0600

    sell history page updated

--- frontend/src/components/SellHistory.js
+++ frontend/src/components/SellHistory.js
@@ -1,25 +1,61 @@
 import React from "react";
 
 const SellHistory = ({ sellRecord }) => {
+  let id = 1;
+  let totalSum = 0;
+
   return (
-    <div className="max-w-md  bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-3">
+    <div className="max-w-md bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-3">
       <div className="p-8">
-        <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
-          Sell Record
+        <div className="tracking-wide text-indigo-500 font-semibold">
+          ID: {sellRecord._id}
         </div>
-        {sellRecord.soldProducts.map((soldProduct, index) => (
-          <div key={index}>
-            <p className="block mt-1 text-lg leading-tight font-medium text-black">
-              Name: {soldProduct.product.name}
-            </p>
-            <p className="mt-2 text-gray-500">
-              Quantity Sold: {soldProduct.quantity}
-            </p>
-            <p className="mt-2 text-gray-500">
-              Unit Price: {soldProduct.product.price}
-            </p>
-          </div>
-        ))}
+        <p className="mt-2 text-gray-500">
+          Date: {new Date(sellRecord.createdAt).toLocaleDateString("en-GB")}
+        </p>
+        {/* <div className="mt-6 uppercase tracking-wide text-sm  font-semibold">
+          Items:
+        </div> */}
+        <table className="table-auto w-full mt-2">
+          <thead>
+            <tr>
+              <th className="px-4 py-2">No.</th>
+              <th className="px-4 py-2">Item</th>
+              <th className="px-4 py-2">Quantity Sold</th>
+              <th className="px-4 py-2">Unit Price</th>
+              <th className="px-4 py-2">Total Amount</th>
+            </tr>
+          </thead>
+          <tbody>
+            {sellRecord.soldProducts.map((soldProduct, index) => {
+              const totalAmount =
+                soldProduct.quantity * soldProduct.product.price;
+              totalSum += totalAmount;
+              return (
+                <tr key={index}>
+                  <td className="border px-4 py-2">{id++}</td>
+                  <td className="border px-4 py-2">
+                    {soldProduct.product.name}
+                  </td>
+                  <td className="border px-4 py-2">{soldProduct.quantity}</td>
+                  <td className="border px-4 py-2">
+                    {soldProduct.product.price}
+                  </td>
+                  <td className="border px-4 py-2">{totalAmount}</td>
+                </tr>
+              );
+            })}
+            <tr>
+              <td
+                colSpan="4"
+                className="border px-4 py-2 text-right font-semibold"
+              >
+                Total:
+              </td>
+              <td className="border px-4 py-2">{totalSum}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
