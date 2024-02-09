d1cb446c533ef282d3fc356caf46e274285992ff
nadimat-sham <1905059@ugrad.cse.buet.ac.bd>
2024-02-09 23:44:49 +0600

    product edit added and sidebar updated

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -2,6 +2,11 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isHistoryOpen, setIsHistoryOpen] = useState(false);
+  const [isEmployeeManagementOpen, setIsEmployeeManagementOpen] =
+    useState(false);
+  const [isFactoryManagementOpen, setIsFactoryManagementOpen] = useState(false);
   return (
     <div className="flex">
       <div className="h-screen w-64 bg-gray-800 p-4 fixed">
@@ -13,78 +18,137 @@ const Sidebar = () => {
         </Link>
 
         <div className="text-white text-lg ">
-
-          <div className="text-red-500 w-full py-2 px-4">
+          <div
+            className="text-red-500 w-full py-2 px-4 hover:bg-gray-700 cursor-pointer"
+            onClick={() => setIsOpen(!isOpen)}
+          >
             Sell Management
           </div>
-          <div className="w-full py-2 px-4 hover:bg-gray-700">
-            <Link to="/products">Products</Link>
+          {isOpen && (
+            <>
+              <div className="w-full py-2 px-4 hover:bg-gray-700">
+                <Link to="/products" className="block w-full h-full">
+                  Products
+                </Link>
+              </div>
+
+              <div className="w-full py-2 px-4 hover:bg-gray-700">
+                <Link to="/customers" className="block w-full h-full">
+                  Customers
+                </Link>
+              </div>
+
+              <div className="w-full py-2 px-4 hover:bg-gray-700">
+                <Link to="/sell" className="block w-full h-full">
+                  Sell
+                </Link>
+              </div>
+
+              <div className="w-full py-2 px-4 hover:bg-gray-700">
+                <Link to="/order" className="block w-full h-full">
+                  Order
+                </Link>
+              </div>
+
+              <div className="w-full py-2 px-4 hover:bg-gray-700">
+                <Link to="/receive" className="block w-full h-full">
+                  Receive
+                </Link>
+              </div>
+            </>
+          )}
+
+          <div
+            className="text-red-500 w-full py-2 px-4 hover:bg-gray-700 cursor-pointer"
+            onClick={() => setIsHistoryOpen(!isHistoryOpen)}
+          >
+            History
           </div>
 
-          <div className="w-full py-2 px-4 hover:bg-gray-700">
-            <Link to="/customers">Customers</Link>
-          </div>
+          {isHistoryOpen && (
+            <>
+              <div className="w-full py-2 px-4 hover:bg-gray-700">
+                <Link to="/sell-history" className="block w-full h-full">
+                  Sell History
+                </Link>
+              </div>
 
-          <div className="w-full py-2 px-4 hover:bg-gray-700">
-            <Link to="/sell">Sell</Link>
-          </div>
+              <div className="w-full py-2 px-4 hover:bg-gray-700">
+                <Link to="/order-history" className="block w-full h-full">
+                  Order History
+                </Link>
+              </div>
 
-          <div className="w-full py-2 px-4 hover:bg-gray-700">
-            <Link to="/order">Order</Link>
-          </div>
+              <div className="w-full py-2 px-4 hover:bg-gray-700">
+                <Link to="/receive-history" className="block w-full h-full">
+                  Receive History
+                </Link>
+              </div>
+            </>
+          )}
 
-          <div className="w-full py-2 px-4 hover:bg-gray-700">
-            <Link to="/receive">Receive</Link>
-          </div>
-
-          <div className="w-full py-2 px-4 hover:bg-gray-700">
-            <Link to="/sell-history">Sell History</Link>
-          </div>
-
-          <div className="w-full py-2 px-4 hover:bg-gray-700">
-            <Link to="/order-history">Order History</Link>
-          </div>
-
-          <div className="w-full py-2 px-4 hover:bg-gray-700">
-            <Link to="/receive-history">Receive History</Link>
-          </div>
-
-          <div className="text-red-500 w-full py-2 px-4">
+          <div
+            className="text-red-500 w-full py-2 px-4 hover:bg-gray-700 cursor-pointer"
+            onClick={() =>
+              setIsEmployeeManagementOpen(!isEmployeeManagementOpen)
+            }
+          >
             Employee Management
           </div>
 
-          <div className="w-full py-2 px-4 hover:bg-gray-700">
-            <Link to="/employees">Employees</Link>
-          </div>
+          {isEmployeeManagementOpen && (
+            <>
+              <div className="w-full py-2 px-4 hover:bg-gray-700">
+                <Link to="/employees" className="block w-full h-full">
+                  Employees
+                </Link>
+              </div>
 
-          <div className="w-full py-2 px-4 hover:bg-gray-700">
-            <Link to="/accounts">Accounts</Link>
-          </div>
+              <div className="w-full py-2 px-4 hover:bg-gray-700">
+                <Link to="/accounts" className="block w-full h-full">
+                  Accounts
+                </Link>
+              </div>
 
-          <div className="w-full py-2 px-4 hover:bg-gray-700">
-            <Link to="/employeesAttendance">Attendance</Link>
-          </div>
+              <div className="w-full py-2 px-4 hover:bg-gray-700">
+                <Link to="/employeesAttendance" className="block w-full h-full">
+                  Attendance
+                </Link>
+              </div>
 
-          <div className="w-full py-2 px-4 hover:bg-gray-700">
-            <Link to="/employeesPerformance">Performance</Link>
-          </div>
+              <div className="w-full py-2 px-4 hover:bg-gray-700">
+                <Link
+                  to="/employeesPerformance"
+                  className="block w-full h-full"
+                >
+                  Performance
+                </Link>
+              </div>
+            </>
+          )}
 
-          <div className="text-red-500 w-full py-2 px-4">
+          <div
+            className="text-red-500 w-full py-2 px-4 hover:bg-gray-700 cursor-pointer"
+            onClick={() => setIsFactoryManagementOpen(!isFactoryManagementOpen)}
+          >
             Factory Management
           </div>
 
-          <div className="w-full py-2 px-4 hover:bg-gray-700">
-            <Link to="/products">Raw Materials</Link>
-          </div>
+          {isFactoryManagementOpen && (
+            <>
+              <div className="w-full py-2 px-4 hover:bg-gray-700">
+                <Link to="/rawMaterials" className="block w-full h-full">
+                  Raw Materials
+                </Link>
+              </div>
 
-          <div className="w-full py-2 px-4 hover:bg-gray-700">
-            <Link to="/products">Products</Link>
-          </div>
-
-
-
-
-          
+              <div className="w-full py-2 px-4 hover:bg-gray-700">
+                <Link to="/products" className="block w-full h-full">
+                  Products
+                </Link>
+              </div>
+            </>
+          )}
         </div>
       </div>
       {/* <div className="flex-grow p-4">{renderComponent()}</div> */}
